7dfe60344d60241ccb8a7ab606b669f54672ac08
Celin <limarcoverde@gmail.com>
2021-10-06 21:22:31 -0300

    run first test

---
+++ app.py
@@ -0,0 +1,173 @@
+from functions import*
+
+VALID_USERNAME_PASSWORD_PAIRS = {
+    'garoa': '123',
+    'startup':'123',
+    'investidor':'123',
+}
+
+
+app = dash.Dash(__name__)
+server = app.server
+
+auth = dash_auth.BasicAuth(
+    app,
+    VALID_USERNAME_PASSWORD_PAIRS
+)
+
+app.title = "AgroData"
+
+tabs_styles,tab_style,tab_selected_style = tabStyles()
+
+app.layout = html.Div([
+                createHeader(),
+                dcc.Tabs(
+                        id="tabs-styled-with-inline", 
+                        value='tab-1', 
+                        children=[
+                                dcc.Tab(
+                                        label='Inserção dos dados', 
+                                        children=[
+                                                html.Div([
+                                                        CreateMenu(
+                                                                className="menu-header",
+                                                                children=[
+                                                                        html.Div(
+                                                                                className='drops',
+                                                                                children=[
+                                                                                        html.H5("Digite o nome da nova Startup"),
+                                                                                        dcc.Input(id='my-input',placeholder='Enter a value...',value='',type='text'),
+                                                                                        html.H3("Empreendedorismo"),
+                                                                                        html.H5("A Startup entende como o mercado opera?"),
+                                                                                        dcc.Dropdown(
+                                                                                                id="drop1",
+                                                                                                options=[
+                                                                                                        {'label': 'Sim.', 'value': 'True'},
+                                                                                                        {'label': 'não.', 'value': 'False'},
+                                                                                                ],
+                                                                                                searchable=False
+                                                                                        ),
+                                                                                        html.H5("A startup consegue construir um produto?"),
+                                                                                        dcc.Dropdown(
+                                                                                                id="drop2",
+                                                                                                options=[
+                                                                                                        {'label': 'Sim.', 'value': 'True'},
+                                                                                                        {'label': 'não.', 'value': 'False'},
+                                                                                                ],
+                                                                                                searchable=False
+                                                                                        ),
+                                                                                        html.H5("A startup consegue explicar a transformação?"),
+                                                                                        dcc.Dropdown(
+                                                                                                id="drop3",
+                                                                                                options=[
+                                                                                                        {'label': 'Sim.', 'value': 'True'},
+                                                                                                        {'label': 'não.', 'value': 'False'},
+                                                                                                ],
+                                                                                                searchable=False
+                                                                                        ),
+                                                                                        html.H5("A startup experenciou problema?"),
+                                                                                        dcc.Dropdown(
+                                                                                                id="drop4",
+                                                                                                options=[
+                                                                                                        {'label': 'Sim.', 'value': 'True'},
+                                                                                                        {'label': 'não.', 'value': 'False'},
+                                                                                                ],
+                                                                                                searchable=False
+                                                                                        ),
+                                                                                        html.H5("A startup consegue evidênciar a solução?"),
+                                                                                        dcc.Dropdown(
+                                                                                                id="drop5",
+                                                                                                options=[
+                                                                                                        {'label': 'Sim.', 'value': 'True'},
+                                                                                                        {'label': 'não.', 'value': 'False'},
+                                                                                                ],
+                                                                                                searchable=False
+                                                                                        ),
+                                                                                        html.Button('Submit', id='submit-val', n_clicks=0),
+                                                                                ]
+                                                                        )       
+                                                                ]
+                                                        )
+                                                ])
+                                        ],
+                                        style=tab_style, 
+                                        selected_style=tab_selected_style
+                                ),
+                                                
+                                dcc.Tab(
+                                        label='Visualização dos dados', 
+                                        children=[
+                                                html.Div(
+                                                        className='tab2',
+                                                        children=[
+                                                                html.H5("Grafico dos Empreendedores"),
+                                                                dcc.Graph(
+                                                                        id='example-graph-2'
+                                                                ),
+                                                                html.Button('Atualizar o Grafico', id='submit-val1', n_clicks=0),
+                                                        ]
+                                                ),
+                                        ],
+                                        style=tab_style, 
+                                        selected_style=tab_selected_style
+                                ),
+                        ], 
+                        style=tabs_styles
+                ),
+                html.Div(id="container-button-timestamp"),
+        ]
+)
+
+@app.callback(Output('submit-val', 'n_clicks'),
+              Input('my-input', 'value'),
+              Input('drop1', 'value'),
+              Input('drop2', 'value'),
+              Input('drop3', 'value'),
+              Input('drop4', 'value'),
+              Input('drop5', 'value'),
+              Input('submit-val', 'n_clicks')
+              )
+def insertion(inputStartup,drop1,drop2,drop3,drop4,drop5,n_clicks):
+        if (n_clicks == 1):
+                flag = check(inputStartup,drop1,drop2,drop3,drop4,drop5)
+                if(flag == 0):
+                        write(inputStartup,drop1,drop2,drop3,drop4,drop5)
+                else:
+                        print("Deu ruim pai")
+        n_clicks = 0
+        return n_clicks
+
+@app.callback(Output('example-graph-2', 'figure'),
+              Input('submit-val1', 'n_clicks'))
+def figGraph(n_clicks):
+        listStartup = []
+        listCount = []
+        count = 0
+        df1 = pd.read_csv("data/db.csv")
+        listStartup = df1["Startups"]
+        stats1 = df1["Entender_como_mercado_opera"]
+        stats2 = df1["Consegue_construir_produto"]
+        stats3 = df1["Explicar_transformacao"]
+        stats4 = df1["Experenciou_problema"]
+        stats5 = df1["Evidencia_de_Solucao"]
+
+        
+        for i in range(len(listStartup)):
+                if(stats1[i] == True):
+                        count += 1
+                if(stats2[i] == True):
+                        count += 1
+                if(stats3[i] == True):
+                        count += 1
+                if(stats4[i] == True):
+                        count += 1
+                if(stats5[i] == True):
+                        count += 1
+                listCount.append(count)
+                count = 0
+
+        fig = go.Figure(data=[go.Scatter(x=listStartup, y=listCount)])
+        return fig
+
+if __name__ == '__main__':
+    app.run_server()
---
+++ functions.py
@@ -0,0 +1,92 @@
+import dash
+import csv
+import dash_auth
+import pandas as pd
+import plotly.graph_objs as go
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.html.Div import Div
+from dash.html.H1 import H1
+from dash.dependencies import Input, Output
+
+
+def createHeader():
+        return html.Div(
+                className="header",
+                children=[
+                        #html.P(children="🌵", className="header-emoji"),
+                        html.H1(
+                                className="header-title",
+                                children="📈AgroData", 
+                        ),
+                        html.P(
+                                className="header-description",
+                                children="Uma plataforma para mudar tudo já visto em análise de dados",
+                        ),
+                ],
+        )        
+
+def CreateMenu(children,className="menu-body"):
+        return html.Div(
+                className=className,
+                children=children
+        )
+        
+def tabStyles ():
+        tabs_styles = {
+                'height': '44px'
+        }
+        tab_style = {
+                'borderBottom': '1px solid #d6d6d6',
+                'padding': '6px',
+                'fontWeight': 'bold'
+        }
+        tab_selected_style = {
+        'borderTop': '1px solid #d6d6d6',
+        'borderBottom': '1px solid #d6d6d6',
+        'backgroundColor': '#119DFF',
+        'color': 'white',
+        'padding': '6px'
+        }
+
+        return tabs_styles,tab_style,tab_selected_style
+
+def check(inputStartup,drop1,drop2,drop3,drop4,drop5):
+        flag = 0
+        if(inputStartup == ''):
+                flag = 1
+        if(drop1 is None):
+                flag = 1
+        if(drop2 is None):
+                flag = 1
+        if(drop3 is None):
+                flag = 1
+        if(drop4 is None):
+                flag = 1
+        if(drop5 is None):
+                flag = 1
+                
+        return flag
+
+def write(inputStartup,ecmo,ccp,et,ep,es):
+        df = pd.read_csv("data/db.csv")
+        listaRespostas = [ecmo, ccp, et, ep, es]
+        for x in range (len(listaRespostas)):
+                if listaRespostas[x] == 'False':
+                        listaRespostas[x] = False
+                else:
+                        listaRespostas[x] = True
+
+        data = {'Startups': [inputStartup],
+          'Entender_como_mercado_opera': [listaRespostas[0]],
+          'Consegue_construir_produto': [listaRespostas[1]],
+          'Explicar_transformacao': [listaRespostas[2]],
+          'Experenciou_problema': [listaRespostas[3]],
+          'Evidencia_de_Solucao': [listaRespostas[4]]
+        }
+
+        df2 = pd.DataFrame(data)
+
+        df = df.append(df2, ignore_index = True)
+        
+        df.to_csv("data/db.csv",index=False)
